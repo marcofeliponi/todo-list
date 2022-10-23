60e6be63565168f13ae3bdc83ebcf5471c96d2d5
Marco Antonio <79797841+marcofeliponi@users.noreply.github.com>
2022-10-23 20:52:25 -0300

    feat: initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:todo_list_app/screens/homepage.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            textTheme: GoogleFonts.nunitoSansTextTheme(
+          Theme.of(context).textTheme,
+        )),
+        home: Homepage()
+    );
+  }
+}
